398be83dd127121ce4e767cb7416edd69769af58
HolyMartianEmpire <super_taniyan@live.jp>
2021-03-01 16:41:42 +0900

    リソース初期化/破棄の隠蔽

--- NJTalk/Program.cs
+++ NJTalk/Program.cs
@@ -14,28 +14,28 @@ namespace NJTalk
             if (args.Length < 1)
                 return;
 
-            OpenJTalk ojt = new OpenJTalk()
+            using (OpenJTalk ojt = new OpenJTalk())
             {
-                mecab_path = ConfigurationManager.AppSettings["dictionary"],
-                voice_path = ConfigurationManager.AppSettings["voice"],
-                alpha = double.Parse(ConfigurationManager.AppSettings["alpha"]),
-                beta = double.Parse(ConfigurationManager.AppSettings["beta"]),
-                fperiod = int.Parse(ConfigurationManager.AppSettings["fperiod"]),
-                sampling_rate = int.Parse(ConfigurationManager.AppSettings["sampling_rate"]),
-                uv_threshold = double.Parse(ConfigurationManager.AppSettings["uv_threshold"]),
-            };
+                ojt.mecab_path = ConfigurationManager.AppSettings["dictionary"];
+                ojt.voice_path = ConfigurationManager.AppSettings["voice"];
+                ojt.alpha = double.Parse(ConfigurationManager.AppSettings["alpha"]);
+                ojt.beta = double.Parse(ConfigurationManager.AppSettings["beta"]);
+                ojt.fperiod = int.Parse(ConfigurationManager.AppSettings["fperiod"]);
+                ojt.sampling_rate = int.Parse(ConfigurationManager.AppSettings["sampling_rate"]);
+                ojt.uv_threshold = double.Parse(ConfigurationManager.AppSettings["uv_threshold"]);
 
-            //設定値反映
-            ojt.load();
+                //設定値反映
+                ojt.load();
 
-            if( args.Length >= 2)
-            {
-                var wave_path = args[1];
-                ojt.synthesis(args[0], wave_path);
-            }
-            else
-            {
-                ojt.talk(args[0]);
+                if (args.Length >= 2)
+                {
+                    var wave_path = args[1];
+                    ojt.synthesis(args[0], wave_path);
+                }
+                else
+                {
+                    ojt.talk(args[0]);
+                }
             }
         }
     }
